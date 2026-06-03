b492ded0826c3d09eccd861694c7131d4cf761f9
VationCode <97288892+VationCode@users.noreply.github.com>
2026-06-03 19:32:44 +0900

    111 ~ 127편
    [ 작업 내용 ]
    플레이어 움직임, 데미지 전달, Gun 모델 임포트 + 셋팅 + 스크립트 + 이펙트

---
+++ TPS Start Project/Assets/02Scripts/PlayerMovement.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController m_characterController;
+    private PlayerInput m_playerInput;
+    private Animator m_animator;
+
+    [SerializeField]
+    private Camera m_followCam;
+    
+    public float Speed = 6f;
+    public float JumpVelocity = 8f;
+    [Range(0.01f, 1f)] public float AirControlPercent;
+
+    public float SpeedSmoothTime = 0.1f;
+    public float TurnSmoothTime = 0.1f;
+    
+    private float m_speedSmoothVelocity;
+    private float m_turnSmoothVelocity;
+    
+    private float m_currentVelocityY;
+
+    [SerializeField]
+    private float m_gravity = -9.8f;
+    // m_characterController 실제 움직임이고 있는 값을 기반으로한 속력값
+    public float CurrentSpeed =>
+        new Vector2(m_characterController.velocity.x, m_characterController.velocity.z).magnitude;
+
+    private void Awake()
+    {
+        m_characterController = GetComponent<CharacterController>();
+        m_playerInput = GetComponent<PlayerInput>();
+        m_animator = GetComponentInChildren<Animator>();
+        m_followCam = Camera.main;
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (CurrentSpeed > 0.2f || m_playerInput.fire) Rotate();
+
+        Move(m_playerInput.moveInput);
+        
+        if (m_playerInput.jump) Jump();
+    }
+
+    private void Update()
+    {
+        UpdateAnimation(m_playerInput.moveInput);
+    }
+
+    public void Move(Vector2 p_moveInput)
+    {
+        // 속력 및 방향
+        var targetSpeed = Speed * p_moveInput.magnitude;
+        var moveDiection = Vector3.Normalize(transform.forward * p_moveInput.y + transform.right * p_moveInput.x);
+
+        // 스피드 부드럽게 전환 , 공중에서는 움직임 값 변화 거의 없게
+        var smoothTime = m_characterController.isGrounded ? SpeedSmoothTime : SpeedSmoothTime / AirControlPercent;
+
+        targetSpeed = Mathf.SmoothDamp(CurrentSpeed, targetSpeed, ref m_speedSmoothVelocity, smoothTime);
+
+        // 속도Y 즉, 중력값 
+        m_currentVelocityY += Time.deltaTime * m_gravity;
+        var velocity = moveDiection * targetSpeed + Vector3.up * m_currentVelocityY;
+
+        m_characterController.Move(velocity * Time.deltaTime);
+
+        if (m_characterController.isGrounded) m_currentVelocityY = 0f;
+    }
+
+    public void Rotate()
+    {
+        var targetRot = m_followCam.transform.eulerAngles.y;
+
+        targetRot = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRot,ref m_turnSmoothVelocity, TurnSmoothTime);
+
+        transform.eulerAngles = Vector3.up * targetRot;
+    }
+
+    public void Jump()
+    {
+        if (!m_characterController.isGrounded) return;
+        m_currentVelocityY = JumpVelocity;
+    }
+
+    private void UpdateAnimation(Vector2 p_moveInput)
+    {
+        // CurrentSpeed는 CharacterController 기반이기에 즉, 캐릭터의 움직임 기반
+        // 벽에 부딪혔을 시 CurrentSpeed는 0이 되기에 그러한 값을 반영하여 벽쪽으로 움직임은 멈춘 애니메이션 반영
+        var animSpeedPer = CurrentSpeed / Speed;
+        m_animator.SetFloat("Vertical Move", p_moveInput.y * animSpeedPer, 0.05f, Time.deltaTime);
+        m_animator.SetFloat("Horizontal Move", p_moveInput.x * animSpeedPer, 0.05f, Time.deltaTime);
+    }
+}
---
+++ TPS Start Project/Assets/02Scripts/PlayerShooter.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+
+public class PlayerShooter : MonoBehaviour
+{
+    public enum AimState
+    {
+        Idle,
+        HipFire
+    }
+
+    public AimState aimState { get; private set; }
+
+    public Gun gun;
+    public LayerMask excludeTarget;
+    
+    private PlayerInput playerInput;
+    private Animator playerAnimator;
+    private Camera playerCamera;
+    
+    private Vector3 aimPoint;
+    private bool linedUp => !(Mathf.Abs( playerCamera.transform.eulerAngles.y - transform.eulerAngles.y) > 1f);
+    private bool hasEnoughDistance => !Physics.Linecast(transform.position + Vector3.up * gun.FireTransform.position.y,gun.FireTransform.position, ~excludeTarget);
+    
+    void Awake()
+    {
+        if (excludeTarget != (excludeTarget | (1 << gameObject.layer)))
+        {
+            excludeTarget |= 1 << gameObject.layer;
+        }
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        gun.gameObject.SetActive(true);
+        gun.Setup(this);
+    }
+
+    private void OnDisable()
+    {
+        gun.gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerInput.fire)
+        {
+            Shoot();
+        }
+        else if (playerInput.reload)
+        {
+            Reload();
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void Shoot()
+    {
+
+    }
+
+    public void Reload()
+    {
+        
+    }
+
+    private void UpdateAimTarget()
+    {
+ 
+    }
+
+    private void UpdateUI()
+    {
+        if (gun == null || UIManager.Instance == null) return;
+        
+        UIManager.Instance.UpdateAmmoText(gun.MagAmmo, gun.AmmoRemain);
+        
+        UIManager.Instance.SetActiveCrosshair(hasEnoughDistance);
+        UIManager.Instance.UpdateCrossHairPosition(aimPoint);
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+
+    }
+}
